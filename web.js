2e0b5adedece973f8c02d6811f54dfed0dfbed23
samuelbalcha <samuel.balcha@gmail.com>
2013-07-09 22:27:40 +0000

    file read from disk

--- web.js
+++ web.js
@@ -4,9 +4,15 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    
-    response.send(fs.readFileSync("index.html"));
+       
+    response.send(getContent());
 });
+
+function getContent(){
+    var c  = fs.readFileSync('./index.html', "utf8");
+    console.log(c);
+    return c;
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
